408a37621ad0ec5d538f41b971ee37e6597a693c
YuryRazhkov <yrozhkov1983@gmail.com>
2022-07-28 21:55:25 +0300

    lesson-3

--- framework/views.py
+++ framework/views.py
@@ -10,6 +10,7 @@ class Page:
     def __call__(self, request):
         return '200 OK', render('page.html', date=request.get('date', None))
 
+
 class Contact:
     def __call__(self, request):
         return '200 OK', render('contact.html', date=request.get('date', None))
